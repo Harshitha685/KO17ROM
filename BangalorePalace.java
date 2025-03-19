a1e596dd5f0b7144a6e086a736ad5527fbeb990b
Harshitha685 <harshithas685@gmail.com>
2025-03-19 14:13:21 +0530

    adding files to methods

---
+++ BangalorePalace.java
@@ -0,0 +1,8 @@
+class BangalorePalace{
+
+public static void manageGarbaFunction(){
+System.out.println("started with manageGarbaFunction");
+Community.provideTicket();
+System.out.println("Garba function got over");
+}
+}
