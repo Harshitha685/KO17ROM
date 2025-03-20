ed2a347c73ac780e7b4847141d54b2acd9b5a721
Harshitha685 <harshithas685@gmail.com>
2025-03-20 19:40:36 +0530

    Delete FoodChain.java

--- FoodChain.java
+++
@@ -1,8 +0,0 @@
-class FoodChain{
-
-public static void consumptionOfFood(){
-System.out.println("consumptionOfFood started");
-Eagle.eatSnake();
-System.out.println("consumptionOfFood ended");
-}
-}
