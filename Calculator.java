90b7cb71905c9937bef799eea1c363e3de47035b
Harshitha685 <harshithas685@gmail.com>
2025-03-20 11:20:55 +0530

    adding a file

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+class Calculator{
+
+public static int add(int number,int anotherNumber){
+
+int total = number + anotherNumber;
+return total;
+
+}
+
+public static int sub(int number,int anotherNumber){
+
+int total = number - anotherNumber;
+return total;
+
+}
+public static int multiplication(int number,int anotherNumber){
+int total = number * anotherNumber;
+return total;	
+	
+}
+
+public static int division(int number,int anotherNumber){
+int total = number / anotherNumber;
+return total;
+	
+}
+
+public static int modulus(int number,int anotherNumber){
+	
+int total = number%anotherNumber;
+return total;
+}
+
+}
+
+
